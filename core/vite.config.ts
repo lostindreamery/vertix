4da47528b55492fbcd0299cd581093e2044fe27d
Gus <58223632+thegu5@users.noreply.github.com>
2026-03-27 23:31:11 -0400

    preserve comments in svelte component html

--- core/vite.config.ts
+++ core/vite.config.ts
@@ -2,12 +2,15 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import type { UserConfig } from "vite";
 
 export default {
-	plugins: [svelte({
-		compilerOptions: {
-			runes: true,
-			hmr: true
-		}
-	})],
+	plugins: [
+		svelte({
+			compilerOptions: {
+				runes: true,
+				hmr: true,
+				preserveComments: true,
+			},
+		}),
+	],
 	build: {
 		target: "esnext",
 	},
